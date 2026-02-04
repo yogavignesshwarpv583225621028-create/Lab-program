51f7ab0eb0ce72339eb66586fbf832d3f4f3ea26
yogavignesshwarpv583225621028-create <yogavignesshwarpv583225621028@nprcolleges.org>
2026-02-04 10:31:49 +0530

    Create Factorial.java

---
+++ Factorial.java
@@ -0,0 +1,9 @@
+class Factorial {
+    public static void main(String[] args) {
+        int fact = 1;
+        for (int i = 1; i <= 5; i++) {
+            fact *= i;
+        }
+        System.out.println("Factorial = " + fact);
+    }
+}
